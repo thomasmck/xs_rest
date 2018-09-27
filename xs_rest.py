eb4b2fe830239b90e3e1db303fc45e968b925af0
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-09-27 19:25:31 +0100

    Removing debug prints
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- xs_rest.py
+++ xs_rest.py
@@ -1,7 +1,7 @@
-from flask import Flask, abort, make_response, request, jsonify
+from flask import Flask, make_response, request, jsonify
 import json
-from datetime import datetime
 import XenAPI
+# Create settings.py with the host, username and password of the host you want to connect to
 from settings import host, username, password
 app = Flask(__name__)
 
@@ -13,58 +13,43 @@ def not_found(error):
 def internal_error(error):
     return make_response(json.dumps({'error': str(error)}), 500)
 
-@app.route('/VDI', methods=['GET'])
-def get_VDIs():
-    # curl -X GET http://127.0.0.1:5000/VDI
-    # Return all VDIs
-    VDIs = session.xenapi.VDI.get_all()
-    return jsonify({"VDIs": VDIs})
-
 @app.route('/api/<object>', methods=['GET'])
 def get_objects(object):
     # curl -X GET http://127.0.0.1:5000/api/VDI
-    # Return all VDIs
+    # Return all objects of type object
     objects = getattr(session.xenapi, object)
     return jsonify({str(object): objects.get_all()})
 
 @app.route('/api/<object>/<uuid>', methods=['GET'])
 def get_object_by_uuid(object, uuid):
     # curl -X GET http://127.0.0.1:5000/api/VDI/<uuid>
-    # Return all VDIs
+    # Return an object ref from a uuid
     objects = getattr(session.xenapi, object)
     return jsonify({str(object): objects.get_by_uuid(uuid)})
 
 @app.route('/action/<object>/<action>', methods=['POST', 'GET'])
 def get_post_object_action(object, action):
-    # curl -X POST "http://localhost:5000/action/VDI/get_SR?tom=test"
+    # curl -X POST "http://localhost:5000/action/VDI/set_name_label?ref=OpaqueRef:006182f6-9efd-4240-96c4-15ed2adf2257&name_label=test"
     # curl -X POST "http://localhost:5000/action/VDI/get_SR?ref=OpaqueRef:006182f6-9efd-4240-96c4-15ed2adf2257"
-
+    # Perform an action on a given object
     arg = request.args
-    print("ARGS: {}".format(arg))
     try:
         options = ()
         for key, value in arg.items():
-            print("VALUE: {}".format(value))
-            #tuple_value = (value,)
             options += (str(value),)
-        print("ACTION: {}".format(options))
         objects = getattr(session.xenapi, object)
         action = getattr(objects, action)(*options)
-        # If this fails as it was looking for dict try other
 
     except Exception as e:
-        print("ERROR_ERROR: {}".format(e))
+        # If the previous command failed due to incorrect data structure than put parameters in dict instead
         if ("bad __structure" or "PARAMETER_COUNT_MISMATCH") in str(e):
             options = {}
             for key, value in arg.items():
                 options[key] = value
-            print("ACTION: {}".format(options))
             objects = getattr(session.xenapi, object)
             action = getattr(objects, action)(options)
 
-    print("ACTION: {}".format(action))
     return jsonify({str(object): action})
-
 
 if __name__ == '__main__':
     try:
